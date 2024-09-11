ff9da238c9624d4d2532006e44d02fa21be2d473
Aiven-gwt <v_bia_v@mail.ru>
2024-09-11 14:10:34 +0300

    create all delete

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from contextlib import asynccontextmanager
 import uvicorn
 
 from core.config import settings
-from core.models import Base, db_helper
 from api_v1 import router as router_v1
 from fastapi import FastAPI
 from items_views import router as items_router
@@ -12,8 +11,7 @@ from users.views import router as users_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with db_helper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+
     yield
 
 
